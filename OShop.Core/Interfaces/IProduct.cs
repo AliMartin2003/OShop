6420b6b829a307eff289053e2f0bb5085b205d60
HighEnd <HighEnd@DESKTOP-K2V8996>
2025-04-12 11:03:13 +0330

    Add IProduct

---
+++ OShop.Core/Interfaces/IProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OShop.DataLayer.Entities;
+
+namespace OShop.Core.Interfaces
+{
+    public interface IProduct
+    {
+        Task<bool> CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(int id);
+        IEnumerable<Product> GetProducts();
+        Task<Product> GetProductById(int id);
+
+
+    }
+}
